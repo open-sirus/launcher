31285e5f06c15c737975a6fca8f0922e3e15d58b
bondiano <bondiano@yandex-team.ru>
2021-01-11 15:06:55 +0300

    Fix spectron test

--- tests/spectron/electron.spec.ts
+++ tests/spectron/electron.spec.ts
@@ -1,3 +1,13 @@
-test('true', () => {
-  expect(true).toBe(true)
+import spectron from 'spectron'
+import { testWithSpectron } from 'vue-cli-plugin-electron-builder'
+
+jest.setTimeout(50000)
+
+test('a window is created', async () => {
+  const { stdout, url, stopServe, app } = await testWithSpectron(spectron)
+  console.log(`electron:serve returned: ${stdout}`)
+  console.log(`the dev server url is: ${url}`)
+
+  expect(await app.client.getWindowCount()).toBe(1)
+  await stopServe()
 })
